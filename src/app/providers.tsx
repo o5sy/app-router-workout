cc73d83eb11c6909c39d7401fb79b7fc014fc036
o5sy <oosydev@gmail.com>
2025-08-13 23:00:50 +0900

    tanstack query 패키지 업데이트 & provider 수정

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,39 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+  isServer,
+} from "@tanstack/react-query";
 
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-// 캐시 설정
-const queryClient = new QueryClient();
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        // 클라이언트에서 refetch를 피하기 위해 기본값으로 설정
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  }
+  if (!browserQueryClient) {
+    browserQueryClient = makeQueryClient();
+  }
+  return browserQueryClient;
+}
 
 function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
